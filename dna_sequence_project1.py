66f7e15b65ffe5716274c454d7fd27338f90da40
apoorvasd9-web <apoorvasd9@gmail.com>
2026-07-11 13:01:30 +0530

    Add files via upload

---
+++ dna_sequence_project1.py
@@ -0,0 +1,105 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+======================================= RESTART: C:/Users/NI/AppData/Local/Programs/Python/Python313/dna_sequence_project.py ======================================
+=== DNA Sequence Ananlysis System ===
+DNA Sequence: ATGCGTAC
+Data Type: <class 'str'>
+Sequence Length: 8
+
+======================================= RESTART: C:/Users/NI/AppData/Local/Programs/Python/Python313/dna_sequence_project.py ======================================
+=== DNA Sequence Ananlysis System ===
+DNA Sequence: ATGCGTAC
+Data Type: <class 'str'>
+Sequence Length: 8
+A Count: 2
+T Count: 2
+G Count: 2
+C Count: 2
+
+======================================= RESTART: C:/Users/NI/AppData/Local/Programs/Python/Python313/dna_sequence_project.py ======================================
+=== DNA Sequence Ananlysis System ===
+DNA Sequence: ATGCGTAC
+Data Type: <class 'str'>
+Sequence Length: 8
+A Count: 2
+T Count: 2
+G Count: 2
+C Count: 2
+G + C Count: 4
+GC Percentage: 50.0
+
+======================================= RESTART: C:/Users/NI/AppData/Local/Programs/Python/Python313/dna_sequence_project.py ======================================
+=== DNA Sequence Ananlysis System ===
+DNA Sequence: ATGCGTAC
+Data Type: <class 'str'>
+Sequence Length: 8
+A Count: 2
+T Count: 2
+G Count: 2
+C Count: 2
+G + C Count: 4
+GC Percentage: 50.0
+DNA sequence length is valid
+
+======================================= RESTART: C:/Users/NI/AppData/Local/Programs/Python/Python313/dna_sequence_project.py ======================================
+=== DNA Sequence Ananlysis System ===
+DNA Sequence: ATGCGTAC
+Data Type: <class 'str'>
+Sequence Length: 8
+A Count: 2
+T Count: 2
+G Count: 2
+C Count: 2
+G + C Count: 4
+GC Percentage: 50.0
+DNA sequence length is valid
+High GC Content
+
+======================================= RESTART: C:/Users/NI/AppData/Local/Programs/Python/Python313/dna_sequence_project.py ======================================
+=== DNA Sequence Ananlysis System ===
+DNA Sequence: ATGCGTAC
+Data Type: <class 'str'>
+Sequence Length: 8
+A Count: 2
+T Count: 2
+G Count: 2
+C Count: 2
+G + C Count: 4
+GC Percentage: 50.0
+DNA sequence length is valid
+High GC Content
+Moderate GC Content
+>>> 
+======================================= RESTART: C:/Users/NI/AppData/Local/Programs/Python/Python313/dna_sequence_project.py ======================================
+=== DNA Sequence Ananlysis System ===
+DNA Sequence: ATGCGTAC
+Data Type: <class 'str'>
+Sequence Length: 8
+A Count: 2
+T Count: 2
+G Count: 2
+C Count: 2
+G + C Count: 4
+GC Percentage: 50.0
+DNA sequence length is valid
+High GC Content
+Moderate GC Content
+DNA sequence accepted
+>>> 
+======================================= RESTART: C:/Users/NI/AppData/Local/Programs/Python/Python313/dna_sequence_project.py ======================================
+=== DNA Sequence Ananlysis System ===
+DNA Sequence: ATGCGTAC
+Data Type: <class 'str'>
+Sequence Length: 8
+A Count: 2
+T Count: 2
+G Count: 2
+C Count: 2
+G + C Count: 4
+GC Percentage: 50.0
+DNA sequence length is valid
+High GC Content
+Moderate GC Content
+DNA sequence accepted
+Quality Status: Good Quality DNA
