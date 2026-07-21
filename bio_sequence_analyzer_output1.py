29f42473b3b35cfb4ddfbba444e5b1ab3979a557
apoorvasd9-web <apoorvasd9@gmail.com>
2026-07-21 19:34:17 +0530

    Add files via upload

---
+++ bio_sequence_analyzer_output1.py
@@ -0,0 +1,52 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+======================================= RESTART: C:/Users/NI/AppData/Local/Programs/Python/Python313/bio_sequence_project.py ======================================
+====================================
+   Bioinformatics Mini Project
+      Created by Apoorva
+====================================
+
+Before Object Creation
+Sequence Object Created
+
+After Object Creation
+
+===== Final Report =====
+
+Sequence ID : SEQ001
+Sequence : ATGCGTAA
+Length : 8
+Lab : Genome Research Lab
+Analyzing General Sequence...
+DNA Analysis Started
+ATGC Count = 8
+Generating Bioinformatics Report...
+Analysis Completed Successfully
+
+Program Finished
+>>> 
+======================================= RESTART: C:/Users/NI/AppData/Local/Programs/Python/Python313/bio_sequence_project.py ======================================
+====================================
+   Bioinformatics Mini Project
+      Created by Apoorva
+====================================
+
+Before Object Creation
+Sequence Object Created
+
+After Object Creation
+
+===== Final Report =====
+
+Sequence ID : SEQ001
+Sequence : ATGCGTAA
+Length : 8
+Lab : Genome Research Lab
+Analyzing General Sequence...
+DNA Analysis Started
+ATGC Count = 8
+Generating Bioinformatics Report...
+Analysis Completed Successfully
+
+Program Finished
